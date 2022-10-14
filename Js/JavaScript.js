874bc29a7df5373a64dce621cbb0b8b04751cef8
XandyNinfadora <xandriellepessanhax@gmail.com>
2022-10-14 00:10:43 -0300

    codificação e descodificação ca CFCS

--- Js/JavaScript.js
+++ Js/JavaScript.js
@@ -1,59 +1,60 @@
-// Base64
+function codificandoCFeB64() {
+    let cifraCs = document.getElementById("cifraCs").checked;
+    let base64 = document.getElementById("base64").checked;
+    let incremento = parseInt(document.getElementById("incremento").value);
+    if (cifraCs == true) {
+        let codificado = "";
+        let texto = document.getElementById("textoInput").value;
+        let texto4 = removeAcentosEspaços(texto);
+        for (let i = 0; i < texto4.length; i++) {
+            let charCode = texto4[i];
+            let charCodificado = String.fromCharCode((charCode.charCodeAt(0) + incremento + 65) - 65);
+            codificado += charCodificado;
+        }
+        document.getElementById("textoVazio").value = codificado;
+    }
 
-function textoCod(textoInput) {
-    let textoCodMod = removeAcentosEspaços(textoInput);
-    let textoCod = btoa(textoCodMod);
-    let meinhaDiv = document.querySelector('textarea');
-    meinhaDiv.textContent = (textoCod);
+    if (base64 == true) {
+        let texto2 = document.getElementById("textoInput").value;
+        let texto3 = removeAcentosEspaços(texto2);
+        let textoCod = btoa(texto3);
+        document.getElementById("textoVazio").value = textoCod;
+    }
+}
+
+function descodificandoCFeB64() {
+    let cifraCs = document.getElementById("cifraCs").checked;
+    let base64 = document.getElementById("base64").checked;
+    if (cifraCs == true) {
+        let incremento = parseInt(document.getElementById("incremento").value);
+        let codificadoDescod = "";
+        let texto4Descod = document.getElementById("textoVazio").value;
+        for (let i = 0; i < texto4Descod.length; i++) {
+            let charCodeDescod = texto4Descod[i];
+            let charCodificadoDescod = String.fromCharCode((charCodeDescod.charCodeAt(0) - incremento - 65) + 65);
+            codificadoDescod += charCodificadoDescod;
+        }
+        document.getElementById("textoVazio").value = codificadoDescod;
+    }
+
+    if (base64 == true) {
+        let texto2Descod = document.getElementById("textoVazio").value;
+        let textoCodDescod = atob(texto2Descod);
+        document.getElementById("textoVazio").value = textoCodDescod;
+    }
 
 }
 
-function textoUnCod(textoCod) {
-    let textoUnCoded = atob(textoCod);
-    let textoCodificado = document.querySelector('textarea');
-    textoCodificado.textContent = (textoUnCoded);
+function nowYouDont() {
+    var sashay = document.getElementById("incremento");
+    sashay.style.visibility = "hidden";
+}
+
+function nowYouSee() {
+    var shantay = document.getElementById("incremento");
+    shantay.style.visibility = "visible";
 }
 
 function removeAcentosEspaços(texto) {
     return texto.normalize("NFD").replace(/[^a-zA-Z\s]/g, "");
-}
-
-
-
-// Cifra de César
-
-// colocar o texto somente em letras maiúsculas para serem convertidas.
-function textoCodCF(textoInputCF) {
-    let textoCodCFMod = removeAcentosEspaços(textoInputCF);
-    let textoCodCF = cifraCesar(textoCodCFMod, 3);
-    let textoCodCFShow = document.querySelector('textarea');
-    textoCodCFShow.textContent = (textoCodCF);
-}
-
-function textoUnCodCF(textoCodCF) {
-    let textoUnCodedCF = cifraCesarUnCod(textoCodCF, 3);
-    let textoCodificadoCF = document.querySelector('textarea');
-    textoCodificadoCF.textContent = (textoUnCodedCF);
-}
-
-
-function cifraCesar(texto, incremento) {
-    let codificado = "";
-    for (let i = 0; i < texto.length; i++) {
-        let charCode = texto[i];
-        let charCodificado = String.fromCharCode((charCode.charCodeAt(0) + incremento + 65) - 65);
-        codificado += charCodificado;
-    }
-    return codificado;
-}
-
-function cifraCesarUnCod(texto, incremento) {
-    let descodificado = "";
-    for (let i = 0; i < texto.length; i++) {
-        let charCodificado = texto[i];
-        let charDescodificado = String.fromCharCode((charCodificado.charCodeAt(0) - incremento - 65) + 65);
-        descodificado += charDescodificado;
-    }
-    return descodificado;
-
 }
